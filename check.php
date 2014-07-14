849db985c991e213507b4f08ac1c9b3ee9b03ade
James Woglom <j@wogloms.net>
2014-07-14 17:54:48 -0400

    finish oop

--- check.php
+++ check.php
@@ -1,95 +1,91 @@
 <?php
+class WeatherChecker {
 
-
-$db = new SQLite3("database.sqlite3");
-
-
-function googl($url) {
-    $c = curl_init();
-    curl_setopt($c, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
-    curl_setopt($c, CURLOPT_URL, "https://www.googleapis.com/urlshortener/v1/url");
-    curl_setopt($c, CURLOPT_POST, 1);
-    curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($c, CURLOPT_SSL_VERIFYHOST, false); // don't check certificate
-    curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($c, CURLOPT_POSTFIELDS, json_encode(array("longUrl" => $url)));
-    $res = curl_exec($c);
-    curl_close($c);
-    $j = json_decode($res);
-    return $j->id;
-
-}
-
-function get_entries() {
-    $url = "http://alerts.weather.gov/cap/wwaatmget.php?x=VAC600&y=0&ts=".time();
-    $urlcontents = file_get_contents($url);
-    $xml = simplexml_load_string($urlcontents);
-    $arr = json_decode(json_encode($xml), TRUE);
-    $entries = $arr['entry'];
-    if(isset($entries['id'])) {
-        $entries = array(0 => $entries);
+    var $db;
+    function __construct() {
+        $this->db = new SQLite3("database.sqlite3");
+        $this->get_entries();
+        $this->loop_entries();
     }
-    return $entries;
-}
 
-function get_message($entry) {
-    $text = $entry['title'];
-    $short = googl($entry['link']['@attributes']['href']);
-    // Keep message under 160 chars
-    if((strlen($text) + strlen($short) + 2) > 160) {
-        $text = substr($text, 0, 157 - (strlen($short) + 2))."...";
-    }
-    $msg = $text.": ".$short;
-    return $msg;
-}
-
-function loop_entries($entries) {
-    foreach($entries as $entry) {
-        $postid = explode("?x=", $entry['id'])[1];
-        $fr = find_row($postid);
-        $msg = get_message($entry);
-        if($fr == 2) add_row($postid, $msg);
-        if($fr >= 1) {
-            notify($msg);
-        } else echo "Not notifying $postid -- already notified. \n";
+    function urlshorten($url) {
+        $c = curl_init();
+        curl_setopt($c, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+        curl_setopt($c, CURLOPT_URL, "https://www.googleapis.com/urlshortener/v1/url");
+        curl_setopt($c, CURLOPT_POST, 1);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($c, CURLOPT_SSL_VERIFYHOST, false); // don't check certificate
+        curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($c, CURLOPT_POSTFIELDS, json_encode(array("longUrl" => $url)));
+        $res = curl_exec($c);
+        curl_close($c);
+        $j = json_decode($res);
+        return $j->id;
 
     }
-}
 
-function find_row($id) {
-    global $db;
-    $query = $db->query("SELECT irc_notified FROM messages WHERE " .
-        "id='" . $db->escapeString($id) . "' LIMIT 1;"
-    );
-    if($query->numColumns() == 0) {
-        return 2;
-    } else {
-        if($query->fetchArray()['irc_notified'] == 0) {
-            return 1;
-        } else {
-            return true;
+    var $entries;
+
+    function get_entries() {
+        $url = "http://alerts.weather.gov/cap/wwaatmget.php?x=VAC600&y=0&ts=".time();
+        $urlcontents = file_get_contents($url);
+        $xml = simplexml_load_string($urlcontents);
+        $arr = json_decode(json_encode($xml), TRUE);
+        $entries = $arr['entry'];
+        if(isset($entries['id'])) {
+            $entries = array(0 => $entries);
+        }
+        $this->entries = $entries;
+    }
+
+    function loop_entries() {
+        foreach($this->entries as $entry) {
+            $postid = explode("?x=", $entry['id'])[1]; // actual ID
+            $fr = $this->find_row($postid); // See if row exists and is notified
+            $msg = $this->get_message($entry);
+            if(true) { //!$fr) {
+                $this->add_row($postid, $msg); // Add row if doesn't exist
+                $this->notify($msg);
+            } else echo "Not notifying $postid -- already notified. \n";
+
         }
     }
+
+    function get_message($entry) {
+        $text = $entry['title'];
+        $short = $this->urlshorten($entry['link']['@attributes']['href']);
+        // Keep message under 160 chars
+        if((strlen($text) + strlen($short) + 2) > 160) {
+            $text = substr($text, 0, 157 - (strlen($short) + 2))."...";
+        }
+        $msg = $text.": ".$short;
+        return $msg;
+    }
+
+    function find_row($id) {
+        $query = $this->db->query("SELECT irc_notified FROM messages WHERE " .
+            "id='" . $this->db->escapeString($id) . "' LIMIT 1;"
+        );
+        return ($query->numColumns() != 0);
+    }
+
+    function add_row($id, $message) {
+        if($this->find_row($id)) return;
+        $this->db->query("INSERT INTO messages VALUES(" .
+            "'" . $this->db->escapeString($id) . "', " .
+            "'" . $this->db->escapeString($message) . "', " .
+            "1);"
+        );
+    }
+
+    function mark_notified($msgid) {
+        $this->db->query("UPDATE TABLE messages SET irc_notified=1 WHERE id='" . $this->db->escapeString($msgid) . "'");
+    }
+
+    function notify($msg) {
+        echo "Notifying: $msg \n";
+    }
 }
 
-function add_row($id, $message) {
-    global $db;
-    echo "INSERT INTO messages VALUES(" .
-        "'" . $db->escapeString($id) . "', " .
-        "'" . $db->escapeString($message) . "', " .
-        "0);";
-    $db->query("INSERT INTO messages VALUES(" .
-        "'" . $db->escapeString($id) . "', " .
-        "'" . $db->escapeString($message) . "', " .
-        "0);"
-    );
-    return true;
-}
-
-function mark_notified($msgid) {
-    global $db;
-    $db->query("UPDATE TABLE messages SET irc_notified=1 WHERE id='" . $db->escapeString($msgid) . "'");
-}
-
-loop_entries(get_entries());
+new WeatherChecker();
 ?>
